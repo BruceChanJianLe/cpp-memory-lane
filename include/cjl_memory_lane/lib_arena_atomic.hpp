ce0a06ab42b0157f6914f53637b5e8abc6802e90
Chan Jian Le <jianle001@e.ntu.edu.sg>
2026-04-04 10:26:49 +0800

    wip: library for arena atomic

---
+++ include/cjl_memory_lane/lib_arena_atomic.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <cassert>
+#include <cstddef>
+#include <atomic>
+#include <cstdlib>
+#include <chrono>
+#include <utility>
+#include <vector>
+#include <thread>
+#include <iostream>
+
+namespace cjl {
+
+  class Orc {
+    char name[4] {'U', 'R', 'G'};
+    int strength = 100;
+    double smell = 1000.0;
+
+  public:
+    static constexpr int NB_MAX = 1'000'000;
+    void * operator new(std::size_t);
+    void * operator new[](std::size_t);
+    void operator delete(void *) noexcept;
+    void operator delete[](void *) noexcept;
+  };
+
+  class Tribe {
+    std::byte *p;
+    std::atomic<std::byte*> curr;
+    static Tribe singleton;
+
+    Tribe() : p{static_cast<std::byte*>(std::malloc(Orc::NB_MAX * sizeof(Orc)))} {
+      assert(p);
+      curr.store(p, std::memory_order_relaxed);
+    }
+
+  public:
+    ~Tribe() {
+      std::free(p);
+    }
+
+    static auto &get() {
+      return singleton;
+    }
+
+    void * allocate_one() {
+      return curr.fetch_add(sizeof(Orc), std::memory_order_relaxed);
+    }
+
+    void * allocate_n(std::size_t n) {
+      return curr.fetch_add(n * sizeof(Orc), std::memory_order_relaxed);
+    }
+
+    void deallocate_one(void *) noexcept {}
+
+    void deallocate_n(void *) noexcept {}
+  };
+} // cjl
