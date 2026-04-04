aad16142a0f60dd31724341f6697075cbf31fb61
Chan Jian Le <jianle001@e.ntu.edu.sg>
2026-04-04 10:27:35 +0800

    wip: library for arena meyer

---
+++ include/cjl_memory_lane/lib_arena_meyer.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <cassert>
+#include <cstddef>
+#include <mutex>
+#include <cstdlib>
+
+namespace cjl {
+
+  class Orc {
+    char name[4] {'U', 'R', 'G'};
+    int strength = 100;
+    double smell = 1000.0;
+
+  public:
+    static constexpr int NB_MAX = 1'000'000;
+    void * operator new(std::size_t);
+    void * operator new[](std::size_t);
+    void operator delete(void *) noexcept;
+    void operator delete[](void *) noexcept;
+  };
+
+  class Tribe {
+    std::mutex m;
+    std::byte *p, *curr;
+
+    Tribe() : p{static_cast<std::byte*>(std::malloc(Orc::NB_MAX * sizeof(Orc)))} {
+      assert(p);
+      curr = p;
+    }
+
+  public:
+    ~Tribe() {
+      std::free(p);
+    }
+
+    static auto &get() {
+      static Tribe singleton;
+      return singleton;
+    }
+
+    void * allocate_one() {
+      std::lock_guard _{m};
+      auto q = curr;
+      curr += sizeof(Orc);
+      return q;
+    }
+
+    void * allocate_n(std::size_t n) {
+      std::lock_guard _{m};
+      auto q = curr;
+      curr += n * sizeof(Orc);
+      return q;
+    }
+
+    void deallocate_one(void *) noexcept {}
+
+    void deallocate_n(void *) noexcept {}
+  };
+} // cjl
---
+++ src/cjl_memory_lane/lib_arena_meyer.cpp
@@ -0,0 +1,19 @@
+#include "cjl_memory_lane/lib_arena_meyer.hpp"
+
+namespace cjl {
+  void * Orc::operator new(std::size_t) {
+    return Tribe::get().allocate_one();
+  }
+
+  void * Orc::operator new[](std::size_t n) {
+    return Tribe::get().allocate_n(n / sizeof(Orc));
+  }
+
+  void Orc::operator delete(void *p) noexcept {
+    Tribe::get().deallocate_one(p);
+  }
+
+  void Orc::operator delete[](void *p) noexcept {
+    Tribe::get().deallocate_n(p);
+  }
+} // cjl 
